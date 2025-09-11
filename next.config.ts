da06fbb0dbfe3d6d8de992be2f70975ee3af7b07
[dyad] <ckcellkom@gmail.com>
2025-09-11 12:43:34 +0700

    [dyad] Configured Supabase image domain - wrote 1 file(s)

--- next.config.ts
+++ next.config.ts
@@ -26,6 +26,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'rsdvsmcvizbmzqeogmpa.supabase.co',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
