665797c0fdb6703bccddb15a2d29b170abebedc2
[dyad] <ckcellkom@gmail.com>
2025-09-14 05:18:09 +0700

    Reverted all changes back to version c56d20e1240b633df85e85195b463df42144a5ca

--- src/hooks/use-auth.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import { useSession } from "@/context/session-context";
-
-export function useAuth() {
-  const { user, signOut, isLoading } = useSession();
-  return { user, signOut, isLoading };
-}
